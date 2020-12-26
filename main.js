9bd8bcbc6cb551e3acc8149c17812dbf715dd0f7
Shaka-W <shakawillock@gmail.com>
2020-12-26 11:17:03 -0500

    Created index, styles, and main files

---
+++ main.js
@@ -0,0 +1,38 @@
+function time() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    let format = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+
+    setClock(hours, minutes, seconds, format);
+};
+
+function setClock(hours, minutes, seconds, format) {
+    const outputHour = document.querySelector('.hour');
+    const outputMinute = document.querySelector('.minute');
+    const outputSecond = document.querySelector('.second');
+    const amOrPm = document.querySelector('.am-pm');
+
+    outputHour.textContent = hours;
+    outputMinute.textContent = minutes;
+    outputSecond.textContent = seconds;
+    amOrPm.textContent = format;
+}
+
+function displayTime() {
+    time();
+    setInterval(time, 1000);
+};
+
+window.addEventListener('load', displayTime);
